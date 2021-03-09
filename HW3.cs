d8803f0fdfee2469f2507a17104337124d9de799
DESKTOP-3HR5KP1\sp_su <supakarn.bell@mail.kmutt.ac.th>
2021-03-09 14:56:16 +0700

    homework3_656

---
+++ HW3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode = Console.ReadLine();
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double t, p;
+
+             if (x < 0)
+            {
+                Console.WriteLine("Invalid mouse position");
+            }
+            else if(mode == "time" && x>0)
+            {   
+                p =  Math.Pow(x - 1, 2);
+                Console.WriteLine("{0},{1}",x,p);
+            }
+            else if(mode == "price" && x>0)
+            {
+                t = Math.Sqrt(y) + 1;
+                Console.WriteLine("{0},{1}", t, y);
+            }            
+            else
+            {
+                Console.WriteLine("Invalid mode");
+            }
+        }
+    }
+}

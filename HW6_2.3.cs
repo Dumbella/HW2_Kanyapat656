bebacc9f00575d9ac7ab48e3c399275f962e9814
Dumbella <78536856+Dumbella@users.noreply.github.com>
2021-06-05 21:22:00 +0700

    HW6_2.3

---
+++ HW6_2.3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW6_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cupcake;
+            Console.Write("Take your makarong do you want to buy : ");
+            cupcake = int.Parse(Console.ReadLine());
+            Console.Write("Large : " + cupcake / 6 + ",");
+            cupcake = cupcake % 6;
+            Console.Write("Medium : " + cupcake / 4 + ",");
+            cupcake = cupcake % 4;
+            if (cupcake != 0)
+            {
+
+                Console.Write("Small : " + cupcake / 2);
+            }
+            else
+            {
+                cupcake++;
+                Console.Write("Small : " + cupcake / 2);
+            }
+        }
+    }
+}
+    
+

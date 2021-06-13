9e4be7b1e458a11f8b746114cf196c3b4a4ed870
Dumbella <78536856+Dumbella@users.noreply.github.com>
2021-06-13 14:33:56 +0700

    ข้อ1

---
+++ 1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace No._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int digit1 = int.Parse(Console.ReadLine());
+            int digit2 = int.Parse(Console.ReadLine());
+            int digit3 = int.Parse(Console.ReadLine());
+            int digit4 = int.Parse(Console.ReadLine());
+            int digit5 = int.Parse(Console.ReadLine());
+            int digit6 = int.Parse(Console.ReadLine());
+            int digit7 = int.Parse(Console.ReadLine());
+            int digit8 = int.Parse(Console.ReadLine());
+            int digit9 = int.Parse(Console.ReadLine());
+
+           
+            int[,] array2 = new int[,] { { digit1, digit2, digit3 }, { digit4, digit5, digit6 },{ digit7, digit8, digit9 } };
+            
+           
+
+
+        }
+    }
+}

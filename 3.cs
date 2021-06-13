48a46ab88d1dca44f6a20ed794949e2b6d14ad2c
Dumbella <78536856+Dumbella@users.noreply.github.com>
2021-06-13 17:03:43 +0700

    ข้อ3..

---
+++ 3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            double[] array = new double[num];
+
+            Console.WriteLine("Please Input Values");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = double.Parse(Console.ReadLine());
+            }
+            string mode = Console.ReadLine();
+            if (mode=="FindMax")
+            {
+
+            }
+            else if (mode=="FindMin")
+            {
+                double min = array[0];
+                for (int i = 0; i < num; i++)
+                {
+                    sum = sum + array[i];
+
+                    if (array[i]<min)
+                    {
+                        min = array[i];
+                    }
+                }
+            }
+            else if (mode == "FindMean")
+            {
+                double sum = 0;
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    
+                        sum = sum + array[i];
+                    
+                }
+                double avg = sum / num;
+                Console.WriteLine("Mean = {0}",avg);
+            }
+            
+            Console.ReadLine();
+
+
+        }
+        
+    }
+}

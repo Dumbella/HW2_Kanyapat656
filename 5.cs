34fd18d20a15534e5e053788353491a85c5093af
Dumbella <78536856+Dumbella@users.noreply.github.com>
2021-06-13 15:07:19 +0700

    ข้อ5

---
+++ 5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numItem = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numItem; i++)
+            {
+                string nameItem = Console.ReadLine();
+                string typeItem = Console.ReadLine();
+
+            }
+            string mode = Console.ReadLine();
+            if (mode == "ShowAll")
+            {
+                for (int i = 1; i <= numItem; i++)
+                {
+                    Console.WriteLine("{0}", nameItem);
+                    Console.WriteLine("{0}", typeItem);
+                    Console.WriteLine();
+
+                }
+            }
+            else if (mode=="End")
+            {
+                Console.ReadLine();
+            }
+        }
+
+
+
+    }
+
+}
